7f5c82d74e7faa5cef1f4ff3bfdcee456e2d1fae
David98 <davidpool270@gmail.com>
2026-04-06 19:40:54 -0600

    Change device version from perilouspike to bomb

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -196,7 +196,7 @@ BOARD_MOVE_GSI_AVB_KEYS_TO_VENDOR_BOOT := true
 TW_LOAD_VENDOR_BOOT_MODULES := true
 
 # Maintainer/Version
-TW_DEVICE_VERSION := perilouspike/beta-1
+TW_DEVICE_VERSION := bomb/duchamp
 
 TW_LOAD_VENDOR_MODULES := "haptic.ko"
 TW_SUPPORT_INPUT_AIDL_HAPTICS := true
